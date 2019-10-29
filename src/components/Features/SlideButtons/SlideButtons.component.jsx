4fa08456f1f70c7aaff73bf7c657d19a55994f51
yirano <yirano@protonmail.ch>
2019-10-29 08:57:04 -0400

    Transfer button mapping from Features to its own component

--- src/components/Features/SlideButtons/SlideButtons.component.jsx
+++ src/components/Features/SlideButtons/SlideButtons.component.jsx
@@ -3,15 +3,11 @@ import '../Features.styles.css';
 
 class SlideButtons extends React.Component {
 	render() {
-		console.log(this.props.item);
 		return (
 			<div>
-				{/* {this.props.item.map(({ id, title, subtitle, caption }) => (
-					<button key={this.props.item.id}>{this.props.item.title}</button>
-				))}
-
-				<div>{this.props.item.subtitle}</div>
-				<div>{this.props.item.caption}</div> */}
+				{this.props.item.map(({ title, id }) => {
+					return <button key={id}>{title}</button>;
+				})}
 			</div>
 		);
 	}
