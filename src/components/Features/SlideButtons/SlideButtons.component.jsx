be86d9fb0acac2b563f00336517a090ad6db3716
yirano <yirano@protonmail.ch>
2019-10-29 08:47:26 -0400

    Commit testing branch for features section

--- src/components/Features/SlideButtons/SlideButtons.component.jsx
+++ src/components/Features/SlideButtons/SlideButtons.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../Features.styles.css';
 
 class SlideButtons extends React.Component {
 	render() {
+		console.log(this.props.item);
 		return (
 			<div>
-				<button>{this.props.title}</button>
+				{/* {this.props.item.map(({ id, title, subtitle, caption }) => (
+					<button key={this.props.item.id}>{this.props.item.title}</button>
+				))}
+
+				<div>{this.props.item.subtitle}</div>
+				<div>{this.props.item.caption}</div> */}
 			</div>
 		);
 	}
