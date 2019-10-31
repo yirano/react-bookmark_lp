e13da61df9af5072e658555b57b189e893f081b0
yirano <yirano@protonmail.ch>
2019-10-30 21:31:37 -0400

    Hide/Display FAQ answers - when its related question is clicked, it'll display the answer to that question and hide the rest

--- src/components/Faqs/Faqs.component.jsx
+++ src/components/Faqs/Faqs.component.jsx
@@ -10,29 +10,43 @@ class Faqs extends React.Component {
 					id: 1,
 					question: 'What is Bookmark?',
 					answer:
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justoeget ultricies fringilla.Phasellus blandit ipsum quis quam ornare mattis.'
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justoeget ultricies fringilla.Phasellus blandit ipsum quis quam ornare mattis.',
+					show: false
 				},
 				{
 					id: 2,
 					question: 'How can I request a new browser?',
 					answer:
-						'Vivamus luctus eros aliquet convallis ultricies.Mauris augue massa, ultricies non ligula.Suspendisse imperdiet.Vivamus luctus eros aliquet convallis ultricies.Mauris augue massa,ultricies non ligula.Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula.Suspendisse imperdiet.'
+						'Vivamus luctus eros aliquet convallis ultricies.Mauris augue massa, ultricies non ligula.Suspendisse imperdiet.Vivamus luctus eros aliquet convallis ultricies.Mauris augue massa,ultricies non ligula.Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula.Suspendisse imperdiet.',
+					show: false
 				},
 				{
 					id: 3,
 					question: 'Is there a mobile app?',
 					answer:
-						'Sed consectetur quam id neque fermentum accumsan.Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget.Cras in ligula quis est pharetra mattis sit amet pharetra purus.Sed sollicitudin ex et ultricies bibendum. '
+						'Sed consectetur quam id neque fermentum accumsan.Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget.Cras in ligula quis est pharetra mattis sit amet pharetra purus.Sed sollicitudin ex et ultricies bibendum. ',
+					show: false
 				},
 				{
 					id: 4,
 					question: 'What about other Chromium browsers?',
 					answer:
-						'Integer condimentum ipsum id imperdiet finibus.Vivamus in placerat mi, at euismod dui.Aliquam vitae neque eget nisl gravida pellentesque non ut velit.'
+						'Integer condimentum ipsum id imperdiet finibus.Vivamus in placerat mi, at euismod dui.Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
+					show: false
 				}
 			]
 		};
 	}
+
+	toggleBody = (id) => {
+		console.log('clicked');
+		console.log(id);
+		this.setState((prevState) => {
+			return this.state.faqs.map((item) => {
+				return item.id === id ? (item.show = !item.show) : (item.show = false);
+			});
+		});
+	};
 	render() {
 		return (
 			<div className="section-4 section">
@@ -43,7 +57,7 @@ class Faqs extends React.Component {
 						to email us.
 					</p>
 				</div>
-				<FaqsQuestions key={this.state.faqs.id} items={this.state.faqs} />
+				<FaqsQuestions key={this.state.faqs.id} items={this.state.faqs} onClick={this.toggleBody} />
 			</div>
 		);
 	}
--- src/components/Faqs/faqs-components/FaqsQuestions.jsx
+++ src/components/Faqs/faqs-components/FaqsQuestions.jsx
@@ -2,28 +2,17 @@ import React, { Component } from 'react';
 import '../Faqs.styles.css';
 
 export class FaqsQuestions extends Component {
-	constructor() {
-		super();
-		this.state = {
-			show: true
-		};
-	}
-	toggleBody = () => {
-		console.log('clicked');
-	};
 	render() {
 		console.log(this.props);
 		return (
 			<div className="faqsContainer caption">
-				{this.props.items.map(({ question, id, answer }) => {
+				{this.props.items.map(({ question, id, answer, show }) => {
 					return (
 						<div key={id} className="faqsContent">
-							<div className="question" onClick={this.toggleBody}>
+							<div className="question" onClick={() => this.props.onClick(id)}>
 								<p>{question}</p>
 							</div>
-							<div className="answer toggleAnswers hide">
-								<p>{answer}</p>
-							</div>
+							<div className="answer toggleAnswers">{show ? <p>{answer}</p> : ''}</div>
 						</div>
 					);
 				})}
