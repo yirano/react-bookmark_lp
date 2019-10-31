05965ffb78213631225dcf1d78d190d9eaf55463
yirano <yirano@protonmail.ch>
2019-10-31 19:58:54 -0400

    Add and style arrow on FAQs questions

--- src/components/Faqs/faqs-components/FaqsQuestions.jsx
+++ src/components/Faqs/faqs-components/FaqsQuestions.jsx
@@ -9,7 +9,20 @@ export class FaqsQuestions extends Component {
 					return (
 						<div key={id} className="faqsContent">
 							<div className="question" onClick={() => this.props.onClick(id)}>
-								<p>{question}</p>
+								<p>
+									{question}
+									<span id="arrowContainer">
+										<svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+											<path
+												id="arrow"
+												fill="none"
+												stroke="hsl(229, 31%, 21%)"
+												strokeWidth="3"
+												d="M1 1l8 8 8-8"
+											/>
+										</svg>
+									</span>
+								</p>
 							</div>
 							<div className="answer toggleAnswers">{show ? <p>{answer}</p> : ''}</div>
 						</div>
