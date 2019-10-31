950ed0857c7a42f767e336ce56614aeb7dd59487
yirano <yirano@protonmail.ch>
2019-10-31 00:52:50 -0400

    Add contents and style footer

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import CallToAction from './components/Cta/Cta.component';
 import Features from './components/Features/Features.component';
 import Download from './components/Download/Download.component';
 import Faqs from './components/Faqs/Faqs.component';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="main">
-				<Nav />
-				<CallToAction />
-				<Features />
-				<Download />
-				<Faqs />
+			<div>
+				<div className="main">
+					<Nav />
+					<CallToAction />
+					<Features />
+					<Download />
+					<Faqs />
+				</div>
+				<Footer />
 			</div>
 		);
 	}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './Footer.css';
+import logo from '../../images/logo-bookmark-footer.svg';
+import twitter from '../../images/icon-twitter.svg';
+import facebook from '../../images/icon-facebook.svg';
+
+export class Footer extends Component {
+	render() {
+		return (
+			<footer>
+				<p className="footerCaption">35,000+ already joined</p>
+
+				<h2 className="footerHeading">Stay up-to-date with what we’re doing</h2>
+
+				<div>
+					<form>
+						<label htmlFor="email" />
+						<input type="email" placeholder="example@email.com" />
+						<button className="contactUs">Contact Us</button>
+					</form>
+				</div>
+
+				<div className="footerBottom">
+					<div className="footerLinks">
+						<ul className="links">
+							<li>
+								<a href="/">
+									<img src={logo} alt="Bookmark Logo" />
+								</a>
+							</li>
+							<li>
+								<a href="/">Features</a>
+							</li>
+							<li>
+								<a href="/">Pricing</a>
+							</li>
+							<li>
+								<a href="/">Contact</a>
+							</li>
+						</ul>
+					</div>
+					<div className="footerSocialLinks">
+						<ul className="socialLinks">
+							<li>
+								<a href="/">
+									<img src={facebook} alt="facebook icon" />
+								</a>
+							</li>
+							<li>
+								<a href="/">
+									<img src={twitter} alt="twitter icon" />
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
