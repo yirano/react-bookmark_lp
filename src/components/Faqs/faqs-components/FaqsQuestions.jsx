28211c5c63cb877763ffa64bc3021f4b63181553
yirano <yirano@protonmail.ch>
2019-10-31 21:22:19 -0400

    Remove redundant const element in FAQs

--- src/components/Faqs/faqs-components/FaqsQuestions.jsx
+++ src/components/Faqs/faqs-components/FaqsQuestions.jsx
@@ -8,12 +8,9 @@ export class FaqsQuestions extends Component {
 				<path id="arrow" fill="none" stroke="hsl(229, 31%, 21%)" strokeWidth="3" d="M1 1l8 8 8-8" />
 			</svg>
 		);
-		const activeArrowStyle = {
-			stroke: 'red'
-		};
 		const SVGarrowActive = (
 			<svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
-				<path id="arrow" fill="none" stroke="red" strokeWidth="3" d="M1 1l8 8 8-8" style={activeArrowStyle} />
+				<path id="arrow" fill="none" stroke="red" strokeWidth="3" d="M1 1l8 8 8-8" />
 			</svg>
 		);
 		const rotate = { transform: 'rotate(180deg)', marginTop: '-6px' };
