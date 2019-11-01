e1dad4fe4c6ce1502902550ced443c2006823723
yirano <yirano@protonmail.ch>
2019-11-01 07:56:00 -0400

    Updates

---
+++ precache-manifest.805ef58bfd018079f804c16fda5e175b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634b5c3f7e5eb34fbf3392fe23b19ca6",
+    "url": "/react-bookmark_lp/index.html"
+  },
+  {
+    "revision": "852884fd675e942b5d4d",
+    "url": "/react-bookmark_lp/static/css/main.cf434893.chunk.css"
+  },
+  {
+    "revision": "c481c346990adc5b8a32",
+    "url": "/react-bookmark_lp/static/js/2.d1626d11.chunk.js"
+  },
+  {
+    "revision": "852884fd675e942b5d4d",
+    "url": "/react-bookmark_lp/static/js/main.cf00b745.chunk.js"
+  },
+  {
+    "revision": "5ed4e529064fce1b681d",
+    "url": "/react-bookmark_lp/static/js/runtime-main.413f5834.js"
+  },
+  {
+    "revision": "7c66c472eb50c9abe027977b6f2b406e",
+    "url": "/react-bookmark_lp/static/media/bg-dots.7c66c472.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/react-bookmark_lp/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "11b0ec333ed9d17690ada04979c489c0",
+    "url": "/react-bookmark_lp/static/media/illustration-features-tab-2.11b0ec33.svg"
+  },
+  {
+    "revision": "7f696303479b76b89e45fcf33b7aff4b",
+    "url": "/react-bookmark_lp/static/media/illustration-features-tab-3.7f696303.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/react-bookmark_lp/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "7c456d71cc9d0e32d18f1487922d3132",
+    "url": "/react-bookmark_lp/static/media/logo-bookmark-footer.7c456d71.svg"
+  },
+  {
+    "revision": "d312eb74c088c1e2cbe7b5637bcd8fda",
+    "url": "/react-bookmark_lp/static/media/logo-bookmark.d312eb74.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/react-bookmark_lp/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/react-bookmark_lp/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/react-bookmark_lp/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
